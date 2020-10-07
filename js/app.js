5358adb3dba0532f502c2c6c0602ef2b702280c8
JustJeo <j.cabrigas.writer@gmail.com>
2020-10-07 15:50:34 -0700

    buttons click

--- js/app.js
+++ js/app.js
@@ -3,14 +3,28 @@ console.log("AYO! This is Jeo!")
 //Declare multiple things when the page loads
     // Buttons to add ingredients
     // Timer stuck at 3:00
-let timer = 15;
+let timer = 10;
     // Current Score set at 0
 let score = 10;
     // High Score set at whatever the current high score is
-let highscore = 0;
+let highscore = 10;
         // NOTE! High score WILL reset with every reload
 
 // Timer Countdown that runs when "begin" button is clicked
+function timeBegin() {
+    const timeInterval = setInterval(() => {
+        if (timer === 0) {
+            game.highscoreCheck()
+            clearInterval(timeInterval);
+            document.querySelector("#timer").removeEventListener("click", beginGame);
+        } else {
+            timer--
+            console.log(timer)
+        }
+        const timerText = document.getElementById("timer")
+        timerText.innerHTML = `Timer: ${timer}s`
+    }, 1000)
+}
 
 // Start Game!
 let game = {
@@ -23,21 +37,23 @@ let game = {
 // Create a "customerBurger" function
     // Creates an array where it ALWAYS starts with a "topBun", randomizes the middle ingredients, ALWAYS has a "patty", and always ends with a "bottomBun"
     customerOrder: function () {
-        let randomOrder = Math.floor(Math.random() * 4);
-        if (randomOrder === 3) {
-            game.customerBurger.splice(1, 0, "onion")
+        let randomOrder = Math.floor(Math.random() * 5);
+        if (randomOrder === 4) {
             game.customerBurger.splice(1, 0, "cheese")
+            game.customerBurger.splice(1, 0, "lettuce")
             game.customerBurger.splice(1, 0, "tomato")
-            game.customerBurger.splice(1, 0, "lettuce")
-        } else if (randomOrder === 2) {
-            game.customerBurger.splice(1, 0, "lettuce")
-            game.customerBurger.splice(1, 0, "cheese")
             game.customerBurger.splice(1, 0, "onion")
-        } else if (randomOrder === 1) {
+        } else if (randomOrder === 3) {
+            game.customerBurger.splice(1, 0, "cheese")
             game.customerBurger.splice(1, 0, "lettuce")
+            game.customerBurger.splice(1, 0, "onion")
+        } else if (randomOrder === 2) {
+            game.customerBurger.splice(1, 0, "cheese")
+            game.customerBurger.splice(1, 0, "lettuce")
+        } else if (randomOrder === 1) {
             game.customerBurger.splice(1, 0, "cheese")
         } else {
-            game.customerBurger.splice(1, 0, "cheese")
+            game.customerBurger
         }
     },
 
@@ -50,13 +66,10 @@ let game = {
         
     },
 
-// document.getElementById("bottomBun").addEventListener("click", buttonClick);
-// document.getElementById("patty").addEventListener("click", buttonClick);
-// document.getElementById("cheese").addEventListener("click", buttonClick);
-// document.getElementById("lettuce").addEventListener("click", buttonClick);
-// document.getElementById("tomato").addEventListener("click", buttonClick);
-// document.getElementById("onion").addEventListener("click", buttonClick);
-// document.getElementById("topBun").addEventListener("click", buttonClick);
+    buttonClick: function() {
+        const gameChoice = [""]
+        console.log("click!")
+    },
 
 // Burger Check!
     orderCheck: function() {
@@ -67,22 +80,13 @@ let game = {
         }
     },
 
-// Time's Done function
-    timeDone: function () {
-        if (timer === 0) {
-            this.highscoreCheck()
-            results.innerHTML = "Here's the results!"
-        }
-        game.highscoreCheck()
-    },
-
     // When timer runs down to 0
         // End Game
         // Compare current score to high score
     highscoreCheck: function () {
         if (score > highscore) {
             highscore = score
-            document.querySelector(".results").innerHTML = "Great job! You got a new high score! You are burger master!"
+            document.querySelector(".results").innerHTML = "Great job! You got a new high score! You are a burger master!"
         } else if ((score > 0) && (score < highscore)) {
             document.querySelector(".results").innerHTML = "You've got some happy customers!"
         } else {
@@ -90,25 +94,19 @@ let game = {
         }
     }
 }
+
     let results = document.createElement("h2")
     results.setAttribute("class", "results")
     results.innerHTML = "Get those orders out!"
     document.body.appendChild(results)
 
-    function timeBegin() {
-        const timeInterval = setInterval(() => {
-            if (timer === 0) {
-                clearInterval(timeInterval);
-                document.querySelector("#timer").removeEventListener("click", beginGame);
-                game.timeDone()
-            } else {
-                timer--
-                console.log(timer)
-            }
-            const timerText = document.getElementById("timer")
-            timerText.innerHTML = `Timer: ${timer}s`
-        }, 1000)
-    }
+    document.getElementById("topBun").addEventListener("click", game.buttonClick);
+    document.getElementById("onion").addEventListener("click", game.buttonClick);
+    document.getElementById("tomato").addEventListener("click", game.buttonClick);
+    document.getElementById("lettuce").addEventListener("click", game.buttonClick);
+    document.getElementById("cheese").addEventListener("click", game.buttonClick);
+    document.getElementById("patty").addEventListener("click", game.buttonClick);
+    document.getElementById("bottomBun").addEventListener("click", game.buttonClick);
 
 const beginGame = document.getElementById("begin")
 beginGame.addEventListener("click", () => {
